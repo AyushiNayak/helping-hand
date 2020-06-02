4d1366da58738b79e2c6b456323e6c2bb425b280
sbarlask <Feb@2019>
2020-06-03 01:12:12 +0530

    Updated DB connections and JPA

---
+++ src/main/java/com/helpinghand/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.helpinghand.repository;
+
+import com.helpinghand.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+  User findById(long id);
+}

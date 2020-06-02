30c8669d62a9e63b5b383ffd71b0bef93ca877c1
sbarlask <Feb@2019>
2020-06-03 01:42:41 +0530

    Added IBM DB2 database

--- src/main/java/com/helpinghand/controller/UserController.java
+++ src/main/java/com/helpinghand/controller/UserController.java
@@ -4,6 +4,7 @@ import com.helpinghand.entity.User;
 import com.helpinghand.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +18,12 @@ public class UserController {
     User user = new User(1L, "sanjida", "barlaskar");
 
     userService.save(user);
-    user = userService.findById(1L);
     return "hello";
+  }
+
+  @GetMapping("/user/{userId}")
+  public User userDetails(@PathVariable("userId") Long userId) {
+    return userService.findById(userId);
   }
 
 }
